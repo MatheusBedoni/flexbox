faa0af14f82f9a1d812d2fe2954379189dcbafd6
unknown <bedonimatheus@gmail.com>
2019-08-26 14:47:09 -0300

    Flexbox

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import flex from './flex';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => flex);
